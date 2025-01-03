97ff1dba82b21cd65eb26fb0884628e73fecce5d
Michael Willingham <michael.willingham@gatech.edu>
2025-01-03 17:39:46 -0600

    Add .config/fish/completions/chezmoi.fish Add .config/fish/completions/kn.fish Add .config/fish/completions/mise.fish Add .config/fish/completions/omnictl.fish Add .config/fish/completions/.fish Add .config/fish/completions/talosctl.fish Add .config/fish/conf.d/.keep Add .config/fish/config.fish Add .config/fish/fish_variables Add .config/fish/functions/c.fish Add .config/fish/functions/cat.fish Add .config/fish/functions/cd.fish Add .config/fish/functions/cls.fish Add .config/fish/functions/k.fish Add .config/fish/functions/ls.fish Add .config/fish/functions/ta.fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,18 @@
+# Configure PATH
+set -U fish_user_paths /usr/local/bin $fish_user_paths
+set -U fish_user_paths /usr/bin $fish_user_paths
+
+# Configure homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Configure mise
+mise activate fish | source
+
+# Enable krew kubectl plugins
+set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin
+
+if status is-interactive
+    zoxide init fish | source
+    macchina -m -p
+    starship init fish | source
+end
---
+++ dot_config/private_fish/functions/c.fish
@@ -0,0 +1,4 @@
+function c --wraps=chezmoi --description 'alias c=chezmoi'
+  chezmoi $argv
+        
+end

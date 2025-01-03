437660c07fb40f67440a16e8ce4429eeb1973e16
Michael Willingham <michael.willingham@gatech.edu>
2025-01-03 17:45:54 -0600

    Add mise install script

---
+++ run_once_after_install-mise.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install mise if it is not already installed
+if ! command -v mise &> /dev/null
+then
+    curl https://mise.run | sh
+fi
+
+# Install all mise packages
+~/.local/bin/mise i -y

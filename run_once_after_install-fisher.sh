bee17ae0b01553265da4faf40dc87f7f39679843
Michael Willingham <michael.willingham@gatech.edu>
2025-01-06 15:37:08 -0600

    asdf

--- run_once_after_install-fisher.sh
+++ run_once_after_install-fisher.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env fish
 
+# Install fisher
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
 # do NOT use tty device for this script
 set -gx TERM xterm-256color
 

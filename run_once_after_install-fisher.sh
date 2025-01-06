991f2a159d4f3493c6403a503e6cbde4968814a8
Michael Willingham <michael.willingham@gatech.edu>
2025-01-06 15:45:22 -0600

    asdfsdaf

--- run_once_after_install-fisher.sh
+++ run_once_after_install-fisher.sh
@@ -2,9 +2,3 @@
 
 # Install fisher
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
-# do NOT use tty device for this script
-set -gx TERM xterm-256color
-
-# Install pure prompt
-fisher install pure-fish/pure

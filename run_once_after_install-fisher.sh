926fa515efacccf33e17ddafe67b32e7cae11650
Michael Willingham <michael.willingham@gatech.edu>
2025-01-06 15:09:17 -0600

    sadf

--- run_once_after_install-fisher.sh
+++ run_once_after_install-fisher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # change to fish shell for the following commands
-chsh -s /usr/bin/fish
+fish
 
 # Install fisher if it is not already installed
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher

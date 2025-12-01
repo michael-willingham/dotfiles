2163d871939cd8fadbd4f2da1b1766f00d3ed899
Michael Willingham <michael@willingham.io>
2025-11-30 20:23:24 -0600

    Update config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,7 +3,7 @@ set -U fish_user_paths /usr/local/bin $fish_user_paths
 set -U fish_user_paths /usr/bin $fish_user_paths
 set -U fish_user_paths ~/.local/bin $fish_user_paths
 
-# Set EDITOR
+# Set EDITOR to helix
 set -Ux EDITOR helix
 
 # Configure homebrew
@@ -14,31 +14,14 @@ end
 # Configure mise
 mise activate fish | source
 
-# Enable krew kubectl plugins
-set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin
-
+# Enable oh-my-posh and zoxide for interactive shells
 if status is-interactive
     oh-my-posh init fish --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/spaceship.omp.json' | source
     zoxide init fish | source
 end
 
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/michael/.lmstudio/bin
-# End of LM Studio CLI section
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" hook $argv | source
-else
-    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH /opt/homebrew/Caskroom/miniconda/base/bin $PATH
-    end
+# More macos specific setup
+if test (uname) = Darwin
+    set -U fish_user_paths ~/.lmstudio/bin $fish_user_paths
+    source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 end
-# <<< conda initialize <<<

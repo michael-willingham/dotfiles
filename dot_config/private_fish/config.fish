811a8f6e36f1febe8f2522b2ff85cc5a635e9e47
Michael Willingham <michael@willingham.io>
2026-03-13 15:36:52 -0500

    update

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,22 @@
+# Configure PATH
+set -U fish_user_paths /usr/local/bin $fish_user_paths
+set -U fish_user_paths /usr/bin $fish_user_paths
+set -U fish_user_paths ~/.local/bin $fish_user_paths
+
+# Set EDITOR to helix
+set -gx EDITOR hx
+set -gx GPG_TTY (tty)
+set -gx XDG_CONFIG_HOME $HOME/.config
+
+# macOS specific setup
+if test (uname) = Darwin
+  eval ( /opt/homebrew/bin/brew shellenv )
+  source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+end
+
+# Enable oh-my-posh and zoxide for interactive shells
+if status is-interactive
+  oh-my-posh init fish --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/paradox.omp.json' | source
+  zoxide init fish | source
+end
+

1551767876f844885e1decc78f01101d7bce9841
Michael Willingham <michael@willingham.io>
2026-03-24 16:02:24 -0500

    update

---
+++ dot_config/private_fish/functions/codex-yolo.fish
@@ -0,0 +1,3 @@
+function codex-yolo --wraps='codex --dangerously-bypass-approvals-and-sandbox' --description 'alias codex-yolo=codex --dangerously-bypass-approvals-and-sandbox'
+    codex --dangerously-bypass-approvals-and-sandbox $argv
+end

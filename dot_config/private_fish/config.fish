f6de2f4e7c4d31929c30934091a9ff0ad70fc02d
Michael Willingham <michael@willingham.io>
2026-03-25 21:12:18 -0500

    Update dotfiles for new machine setup

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,14 +5,43 @@ set -gx EDITOR nvim
 set -gx GPG_TTY (tty)
 set -gx XDG_CONFIG_HOME $HOME/.config
 
+for completion_dir in /opt/nanobrew/prefix/opt/*/share/fish/vendor_completions.d
+    if test -d $completion_dir
+        if not contains -- $completion_dir $fish_complete_path
+            set -ga fish_complete_path $completion_dir
+        end
+    end
+end
+
+for function_dir in /opt/nanobrew/prefix/opt/fish/vendor_functions.d
+    if test -d $function_dir
+        if not contains -- $function_dir $fish_function_path
+            set -ga fish_function_path $function_dir
+        end
+    end
+end
+
+for conf_dir in /opt/nanobrew/prefix/opt/fish/vendor_conf.d
+    if test -d $conf_dir
+        for conf in $conf_dir/*.fish
+            if test -f $conf
+                source $conf
+            end
+        end
+    end
+end
+
+if test -x /opt/nanobrew/prefix/bin/nb
+    /opt/nanobrew/prefix/bin/nb completions fish | source
+end
+
 # macOS specific setup
 if test (uname) = Darwin
-  eval ( /opt/homebrew/bin/brew shellenv )
-  source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+    source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 end
 
 # Enable oh-my-posh and zoxide for interactive shells
 if status is-interactive
-  oh-my-posh init fish --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/spaceship.omp.json' | source
-  zoxide init fish | source
+    oh-my-posh init fish --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/spaceship.omp.json' | source
+    zoxide init fish | source
 end
